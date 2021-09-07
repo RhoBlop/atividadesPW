fddbe452ad7e02bc05fb80935327fdcd44f021e6
Thiago Neves Luz <84537567+RhoBlop@users.noreply.github.com>
2021-09-07 19:08:11 -0300

    Add files via upload

---
+++ tamanhoFonteTitulo.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', criaBotoes)
+// Optei por fazer de uma maneira diferente da do vídeo, para aprender
+const fontSizes = ['0.5em', '1em', '1.5em',
+'2em', '2.5em', '3em', '3.5em', '4em']
+let size = 3
+// Solução alternativa (do vídeo): h1.style.fontSize = `${size}px` (nesse caso seria necessário mudar o increment da var size) 
+function increaseFontSize() {
+    if (size < 7) {
+        const h1 = document.querySelector('h1')
+        size += 1
+        h1.style.fontSize = fontSizes[size]
+    }
+}
+function decreaseFontSize() {
+    if (size > 0) {
+        const h1 = document.querySelector('h1')
+        size -= 1
+        h1.style.fontSize = fontSizes[size]
+    }
+}
+function criaBotoes() {
+    criaBotao('#increase', '#title', '+', 'buttons', increaseFontSize)
+    criaBotao('#decrease', '#title', '-', 'buttons', decreaseFontSize)
+}
+function criaBotao(buttonId, appendElementId, innerText, addClass, clickEvent) {
+    const appendElement = document.querySelector(appendElementId)
+    const button = document.createElement('button')
+    appendElement.append(button)
+    button.id = buttonId
+    button.innerText = innerText
+    button.classList.add(addClass)
+    button.addEventListener('click', clickEvent)
+}
